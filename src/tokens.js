0fb3c0e25b224b3b2701004b086add7dd64dd5b3
Marijn Haverbeke <marijn@haverbeke.nl>
2020-10-23 17:15:58 +0200

    Improve recovery from mismatched close tags

--- src/tokens.js
+++ src/tokens.js
@@ -1,23 +1,46 @@
 /* Hand-written tokenizer for XML tag matching. */
 
 import {ExternalTokenizer} from "lezer"
-import {MatchingTagName, nonMatchingTagName, Element} from "./parser.terms.js"
+import {StartTag, StartCloseTag, mismatchedStartCloseTag, incompleteStartCloseTag,
+        Element} from "./parser.terms.js"
 
 function nameChar(ch) {
   return ch == 45 || ch == 46 || ch == 58 || ch >= 65 && ch <= 90 || ch == 95 || ch >= 97 && ch <= 122 || ch >= 161
 }
 
-export const closeTagName = new ExternalTokenizer((input, token, stack) => {
+function isSpace(ch) {
+  return ch == 9 || ch == 10 || ch == 13 || ch == 32
+}
+
+let elementQuery = [Element], openAt = 0
+
+function parentElement(input, stack, pos, len) {
+  openAt = stack.startOf(elementQuery, pos)
+  if (openAt == null) return null
+  let match = /^<\s*([\.\-\:\w\xa1-\uffff]+)/.exec(input.read(openAt, openAt + len + 10))
+  return match ? match[1].toLowerCase() : ""
+}
+
+export const startTag = new ExternalTokenizer((input, token, stack) => {
   let pos = token.start
-  for (;;) {
-    let next = input.get(pos)
-    if (!nameChar(next)) break
-    pos++
-  }
-  if (pos > token.start) {
-    let name = input.read(token.start, pos)
-    let elementStart = stack.startOf([Element])
-    let match = elementStart < 0 ? null : /^<\s*([\.\-\:\w\xa1-\uffff]+)/.exec(input.read(elementStart, elementStart + name.length + 10))
-    token.accept(match && match[1] == name ? MatchingTagName : nonMatchingTagName, pos)
+  if (input.get(pos++) != 60 /* '<' */) return
+  let next = input.get(pos++)
+  if (next == 47 /* '/' */) {
+    let tokEnd = pos
+    while (isSpace(input.get(pos))) pos++
+    let nameStart = pos
+    while (nameChar(input.get(pos))) pos++
+    if (pos == nameStart) return token.accept(incompleteStartCloseTag, tokEnd)
+
+    let name = input.read(nameStart, pos)
+    let parent = parentElement(input, stack, stack.pos + 1, name.length)
+    if (name == parent) return token.accept(StartCloseTag, tokEnd)
+    while (parent != null) {
+      parent = parentElement(input, stack, openAt, name.length)
+      if (parent == name) return
+    }
+    token.accept(mismatchedStartCloseTag, tokEnd)
+  } else if (next != 33 /* '!' */ && next != 63 /* '?' */) {
+    return token.accept(StartTag, token.start + 1)
   }
 }, {contextual: true})
